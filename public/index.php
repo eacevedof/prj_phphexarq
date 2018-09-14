59d4823770e950bcda5cb060cb549ee3ec8775b3
eacevedof <eacevedof@yahoo.es>
2018-09-15 01:08:56 +0200

    homecontroller.index corriendo

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
 require_once("../vendor/autoload.php");
-phpinfo();
+//phpinfo();
+use Phphex\Application\Http\Controllers\HomeController;
+$controller = new HomeController();
+echo $controller->index();
---
+++ src/Application/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Phphex\Application\Http\Controllers;
+
+class HomeController
+{
+    public function __construct()
+    {
+        ;
+    }
+    
+    public function index()
+    {
+        return "Hello World Controller";
+    }
+
+}//HomeController
